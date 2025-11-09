4fc2ef224162b002f7de3cde0fde79ad4023d8f6
JordyRomeroa <jromeroa5@est.ups.edu.ec>
2025-11-09 14:20:20 -0500

    correccion del proyecto y funciones agregadas

--- src/app/features/proyectos-pages/proyectos-pages.ts
+++ src/app/features/proyectos-pages/proyectos-pages.ts
@@ -1,35 +1,42 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
+export interface Proyecto {
+  id: number;
+  nombre: string;
+  descripcion: string;
+}
+
 @Component({
   selector: 'app-proyectos-pages',
-  imports: [],
-  template: `<p>proyectos-pages works!</p>`,
-  styleUrl: './proyectos-pages.css',
+  standalone: true,
+  templateUrl: './proyectos-pages.html',
+  styleUrls: ['./proyectos-pages.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-  export class ProyectosPages {
+export class ProyectosPages {
 
-  name=signal('');
-  description=signal('');
+  name = signal('');
+  description = signal('');
 
-  proyectos =signal<Proyecto[]>([
-    {
-      id: 1, nombre: 'Proyecto A ',
-      descripcion: 'Descripcion '}
-    ]);
+  proyectos = signal<Proyecto[]>([
+    { id: 1, nombre: 'Proyecto A', descripcion: 'Descripcion' }
+  ]);
 
-  changeName(value: string){
-    this.name.set (value);}
+  changeName(value: string) {
+    this.name.set(value);
+  }
 
-  changeDescription(value: string){
-    this.description.set (value);}
+  changeDescription(value: string) {
+    this.description.set(value);
+  }
 
-  addProyecto(){
-    const newProyecto: Proyecto={
-      id: this.proyectos().length +1,
+  addProyecto() {
+    const newProyecto: Proyecto = {
+      id: this.proyectos().length + 1,
       nombre: this.name(),
       descripcion: this.description()
     };
+
     this.proyectos.set([...this.proyectos(), newProyecto]);
     this.name.set('');
     this.description.set('');
